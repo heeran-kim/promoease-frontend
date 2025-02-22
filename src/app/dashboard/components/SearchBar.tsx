6f922e7a465cad88b0821c7603836a52796eeeb5
Heeran Kim <heerankim3@gmail.com>
2025-02-22 14:00:09 +1000

    [SearchBar] Dark mode

--- src/app/dashboard/components/SearchBar.tsx
+++ src/app/dashboard/components/SearchBar.tsx
@@ -4,12 +4,13 @@ import { FaSearch } from "react-icons/fa";
 export default function SearchBar({ setSearchTerm }: { setSearchTerm: (value: string) => void }) {
     return (
         <div className="relative flex-grow">
-            {/* 돋보기 아이콘 (FontAwesome) */}
-            <FaSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 text-sm" />
+            <FaSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 dark:text-gray-300 text-sm" />
             <input
                 type="text"
                 placeholder="Search Restaurants..."
-                className="pl-10 pr-4 py-2 border rounded-lg w-full text-sm"
+                className="pl-10 pr-4 py-2 border rounded-lg w-full text-sm
+                           text-gray-900 bg-white border-gray-300 focus:ring-2 focus:ring-blue-500 focus:outline-none
+                           dark:bg-gray-800 dark:border-gray-600 dark:text-gray-200 dark:placeholder-gray-400 dark:focus:ring-blue-400"
                 onChange={(e) => setSearchTerm(e.target.value)}
             />
         </div>
