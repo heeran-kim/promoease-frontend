8ce340002637117d65c18e48c83866f0f4c07952
Jinwoo Kim <jinunemo@gmail.com>
2025-02-25 21:51:49 +1000

    add missing file

---
+++ src/components/layout/MainNavbar.tsx
@@ -0,0 +1,25 @@
+// src/components/layout/Navbar.tsx
+"use client";
+
+import { useState, useEffect } from "react";
+import Logo from "@/components/navigation/Logo";
+import NavLinks from "@/components/navigation/NavLinks";
+import AuthButtons from "@/components/navigation/AuthButtons";
+import SubNavbar from "@/components/layout/SubNavbar";
+import { useAuth } from "@/components/auth/AuthProvider";
+
+export default function Navbar() {
+    const { user } = useAuth();
+
+    return (
+        <div className={`left-0 w-full`}>
+            <div className="flex justify-between max-w-7xl mx-auto px-6 py-2 flex items-center h-14">
+                <div className="flex items-center gap-2">
+                    <Logo />
+                    {!user && <NavLinks />}
+                </div>
+                <AuthButtons />
+            </div>
+        </div>
+    );
+}
